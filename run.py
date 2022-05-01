445b5ab47bae25b3c8ab978ce4176762964f4068
LisaMolloy5 <molloylisa5@gmail.com>
2022-05-01 14:52:28 +0000

    Add game combinations

--- run.py
+++ run.py
@@ -16,3 +16,18 @@ computerScore = 0
 
 gameChoices = ["Water", "Fire", "Earth"]
 
+def hasWon(playerChoice, computerChoice):
+    """
+    Function to calculate game combinations to show who has won the game.
+    """      
+    if (playerChoice == "Water" and computerChoice == "Fire") or (playerChoice == "Fire" and computerChoice == "Earth") or (playerChoice == "Earth" and computerChoice == "Water"):
+        print("YOU WON! :D")
+        return "Player"
+    elif (playerChoice == "Fire" and computerChoice == "Water") or (playerChoice == "Earth" and computerChoice == "Fire") or (playerChoice == "Water" and computerChoice == "Earth"):
+        print("YOU LOST :(")
+        return "Computer"
+    elif (playerChoice == computerChoice):
+        print("TIE!")
+
+    
+
